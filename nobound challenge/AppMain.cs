d198a0a4a3f4a7c8abfc19f90761216159576914
changsud <a01023853321@gmail.com>
2026-09-08 22:03:35 +0900

    노바운드 챌린지

--- nobound challenge/AppMain.cs
+++ nobound challenge/AppMain.cs
@@ -2,7 +2,7 @@
 // Author: 3dapi (https://github.com/3dapi)
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-//#define ACTIVE_GLC2DLIB
+#define ACTIVE_GLC2DLIB
 
 internal static class AppMain
 {
@@ -11,7 +11,7 @@ internal static class AppMain
 	[STAThread]
 	private static void Main()
 	{
-		ApplicationConfiguration.Initialize();
+		//ApplicationConfiguration.Initialize();
 
 		using GameMain app = new();
 		app.Run();
