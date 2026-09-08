927183ee9ec2c983e0ad44c040b34ce38538ae7f
changsud <a01023853321@gmail.com>
2026-09-08 21:05:00 +0900

    노바운드 챌린지

---
+++ nobound challenge/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace nobound_challenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
