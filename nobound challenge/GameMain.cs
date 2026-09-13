4d483b1402a8f980ec094e301e07fe75ca3687d4
changsud <a01023853321@gmail.com>
2026-09-13 15:04:31 +0900

    노바 챌린지

--- nobound challenge/GameMain.cs
+++ nobound challenge/GameMain.cs
@@ -8,24 +8,28 @@ class GameMain : G2AppBase
 {
 	public override System.Drawing.Size ScreenSize => GameGlobal.ScreenSize;
 	public override string GameName => GameGlobal.GameName;
+    private G2Texture? background;
+    private G2Texture? title;
+    private G2Texture? gamestart;
 
-	protected override void Initialize()
+
+
+    protected override void Initialize()
 	{
-		//---------------------------------------
-		// 게임 관련 객체를 생성합니다.
-		//---------------------------------------
-	}
+
+        
+    //---------------------------------------
+    // 게임 관련 객체를 생성합니다.
+    //---------------------------------------
+    var texuidir = "resource/tex_ui/";
+        background = new G2Texture("resource/배경.png");
+       gamestart = new G2Texture("resource/게임 시작.png");
+       title = new G2Texture("resource/타이틀화면.png");
+    }
 
 	protected override void Update()
 	{
-		double elapsed = TotalTime;
-
-		this.ClearColor = new Color4(
-			red: (float)(Math.Sin(elapsed) * 0.5 + 0.5),
-			green: (float)(Math.Sin(elapsed + Math.PI / 2.0) * 0.5 + 0.5),
-			blue: (float)(Math.Sin(elapsed + Math.PI) * 0.5 + 0.5),
-			alpha: 1.0f);
-
+		
 		//---------------------------------------
 		// 게임 관련 객체를 갱신합니다.
 		//---------------------------------------
@@ -33,16 +37,27 @@ class GameMain : G2AppBase
 
 	protected override void Render()
 	{
-		//---------------------------------------
-		// 게임 관련 객체를 렌더링 합니다.
-		//---------------------------------------
-	}
+        //---------------------------------------
+        // 게임 관련 객체를 렌더링 합니다.
+        //---------------------------------------
+        background?.Draw();
+        title?.Draw();
+        gamestart?.Draw(0, 100);
 
-	public override void Dispose()
+
+    }
+
+    public override void Dispose()
 	{
 		base.Dispose();
-		//---------------------------------------
-		// 게임 관련 객체를 해제합니다.
-		//---------------------------------------
-	}
+        //---------------------------------------
+        // 게임 관련 객체를 해제합니다.
+        //---------------------------------------
+        gamestart?.Draw(0,100);
+        title?.Dispose();
+        background?.Dispose();
+
+
+
+    }
 }
